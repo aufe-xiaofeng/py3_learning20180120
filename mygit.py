c8d6bb7349e10d51a81101c29a0600c19f9e6929
xiaofeng <1194756699@qq.com>
2018-01-23 21:19:21 +0800

    auto update

--- mygit.py
+++ mygit.py
@@ -2,5 +2,5 @@ import os
 
 os.system("cd C:\DDD\soft\git\mygithub\py3_learning20180120")
 os.system("git add .")
-os.system('''git commit -m "tt"''')
+os.system('''git commit -m "auto update"''')
 os.system('''git push -u origin master''')
