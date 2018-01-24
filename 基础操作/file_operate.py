74b493c715b1863851198da4bee54f6fca529108
xiaofeng <1194756699@qq.com>
2018-01-24 19:39:38 +0800

    auto update

--- 基础操作/file_operate.py
+++ 基础操作/file_operate.py
@@ -8,7 +8,21 @@
 # f.write('''你还“喜欢我吗？”\n''')
 # f.close()
 
+'''
 ff = open('test.txt', 'r', encoding='utf-8')
-content = ff.read()
+# content = ff.read()
+# content = ff.readline()
+content = ff.readlines()
 print(content)
 ff.close()
+'''
+
+# fff = open('test.txt', 'r', encoding='utf-8')
+# for line in fff:
+#     print(line.strip('\n'))
+
+#上下文管理器
+with open('test.txt', 'r', encoding='utf-8') as f:
+    content = f.readlines()
+    for line in content:
+        print(line.strip('\n'))
