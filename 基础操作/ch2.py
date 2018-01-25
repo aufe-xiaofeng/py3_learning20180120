cf3d1629fcb765d8a47810f82bddbf9b0a5de72d
xiaofeng <1194756699@qq.com>
2018-01-25 20:25:33 +0800

    auto update

---
+++ 基础操作/ch2.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+# author:xiaofeng
+# 基本数据类型
+import keyword
+
+xiaofeng = 22.0
+# print(xiaofeng)
+# print(type(xiaofeng))
+
+# print(keyword.kwlist)
+a = 'a'
+b = 'b'
+print(a)
+print(b)
+print('-------')
+print(a, end=' ')
+print(b, end=' ')
