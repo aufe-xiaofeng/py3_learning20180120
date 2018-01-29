69ef18c7bbe8a6643803b3806363f92470cd4f22
xiaofeng <1194756699@qq.com>
2018-01-30 01:48:49 +0800

    auto update

--- py_mysql/office_demo.py
+++ py_mysql/office_demo.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 # author:xiaofeng
 
-import pymysql.cursors
+import pymysql
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
@@ -15,16 +15,16 @@ try:
     with connection.cursor() as cursor:
         # Create a new record
         value_tuple = ('aaaaa', 'Mohan', 25, 'M', 2000)
-        sql = "INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) VALUES ('%s', '%s', '%d', '%c', '%d')"
+        sql = "INSERT INTO employee (FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) VALUES (%s, %s, %s, %s, %s)"
+        print(value_tuple)
         cursor.execute(sql, value_tuple)
-
     # connection is not autocommit by default. So you must commit to save
     # your changes.
     connection.commit()
 
     with connection.cursor() as cursor:
         # Read a single record
-        sql = "SELECT * FROM EMPLOYEE WHERE FIRST_NAME = '%s'"
+        sql = "SELECT * FROM employee WHERE FIRST_NAME = %s"
         cursor.execute(sql, ('alan',))
         result = cursor.fetchone()
         print(result)
