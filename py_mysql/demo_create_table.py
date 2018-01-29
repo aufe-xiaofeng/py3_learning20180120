843e90c70a31ae1d184dc822f597244d1bd592b5
xiaofeng <1194756699@qq.com>
2018-01-30 00:00:47 +0800

    auto update

---
+++ py_mysql/demo_create_table.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+# author:xiaofeng
+
+import pymysql
+
+# 打开数据库连接
+db = pymysql.connect("localhost", "root", "root", "testdb")
+
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+
+# 使用 execute() 方法执行 SQL，如果表存在则删除
+cursor.execute("drop table if EXISTS employee")
+
+# # 使用预处理语句创建表
+sql = """CREATE TABLE EMPLOYEE (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,
+         SEX CHAR(1),
+         INCOME FLOAT )"""
+
+cursor.execute(sql)
+
+# 关闭数据库连接
+db.close()
